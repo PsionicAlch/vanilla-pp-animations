3873a843f71a3356eb2288c3921a7719661de896
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2025-10-07 10:48:42 +0200

    Project created

---
+++ build/vanilla-pp-animations/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0efbca805d8a5b4d4e59');
---
+++ build/vanilla-pp-animations/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '457cc16faed92772aef0');
