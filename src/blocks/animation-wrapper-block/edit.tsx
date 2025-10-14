1736d2bb49100e85a76bdd5b81744ab6930401d7
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2025-10-14 20:16:01 +0200

    Started work on setting up editor controls

--- src/blocks/animation-wrapper-block/edit.tsx
+++ src/blocks/animation-wrapper-block/edit.tsx
@@ -1,10 +1,50 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks, InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { Panel, PanelBody, SelectControl } from '@wordpress/components';
+import { BlockEditProps } from '@wordpress/blocks';
 
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }: BlockEditProps<any>) {
+	const domain = 'animation-wrapper-block';
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Animation Wrapper Block - hello from the editor!', 'animation-wrapper-block' ) }
-		</p>
+		<>
+			<InspectorControls>
+				<Panel header={__('Animation Controls', domain)}>
+					<PanelBody title={__('Basics', domain)}>
+						<SelectControl
+							label={__('Type', domain)}
+							value={attributes.animation || 'to'}
+							options={[
+								{ label: __('To', domain), value: 'to' },
+								{ label: __('From', domain), value: 'from' },
+								{ label: __('FromTo', domain), value: 'fromto' },
+								{ label: __('Set', domain), value: 'set' },
+							]}
+							onChange={(value) => setAttributes({ animation: value})}
+							__next40pxDefaultSize
+						/>
+
+						<SelectControl
+							label={__('Target', domain)}
+							value={attributes.target || 'self'}
+							options={[
+								{ label: __('Self', domain), value: 'self' },
+								{ label: __('Children', domain), value: 'children' },
+							]}
+							onChange={(value) => setAttributes({ target: value})}
+							__next40pxDefaultSize
+						/>
+					</PanelBody>
+
+					<PanelBody title={__('Properties', domain)}>
+						Testing 2
+					</PanelBody>
+				</Panel>
+			</InspectorControls>
+
+			<div { ...useBlockProps() }>
+				<InnerBlocks />
+			</div>
+		</>
 	);
 }
--- src/blocks/animation-wrapper-block/save.tsx
+++ src/blocks/animation-wrapper-block/save.tsx
@@ -1,9 +1,9 @@
-import { useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 export default function Save() {
 	return (
-		<p { ...useBlockProps.save() }>
-			{ 'Animation Wrapper Block - hello from the saved content!' }
-		</p>
+		<div { ...useBlockProps.save() }>
+			<InnerBlocks.Content />
+		</div>
 	);
 }
